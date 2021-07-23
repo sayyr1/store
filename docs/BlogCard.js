50f1d0bc03e2aaa35e89bd1e41e8d3a0c2721901
sayyr1 <edwinsay03@gmail.com>
2021-07-23 00:15:15 -0500

    push

---
+++ docs/BlogCard.js
@@ -0,0 +1,11 @@
+import React from "./snowpack/pkg/react.js";
+function BlogsCard() {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "shadow-md items-center w-80"
+  }, /* @__PURE__ */ React.createElement("h2", {
+    className: "text-center"
+  }, "Title"), /* @__PURE__ */ React.createElement("p", {
+    className: "text-center"
+  }, "content"), /* @__PURE__ */ React.createElement("h2", null, "Hola no sabes que estas."));
+}
+export default BlogsCard;
